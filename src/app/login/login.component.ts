6a5f909757bf44b57ed2d3479af07842eced9423
Pablo Angola <pablo.angola97@gmail.com>
2017-07-19 10:53:27 -0500

    Cambio Login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -16,8 +16,6 @@ export class LoginComponent implements OnInit {
   user: Observable<firebase.User>;
   email: string = "";
   password: string = "";
-  //modalRegister = new EventEmitter<string | MaterializeAction>();
-  //modalForget = new EventEmitter<string | MaterializeAction>();
   registerEmail: string;
   registerPassword: string;
   forgetEmail: string;
@@ -26,30 +24,10 @@ export class LoginComponent implements OnInit {
     private router: Router,
     private jwtService: JwtService) {
   }
-  openModal() {
-    //  this.modalRegister.emit({ action: "modal", params: ['open'] });
-  }
-  forgetModal() {
-    //this.modalForget.emit({ action: "modal", params: ['open'] });
-  }
-
-  registerUser() {
-
-    //  var modalRegister = this.modalRegister;
-    firebase.auth().createUserWithEmailAndPassword(this.registerEmail, this.registerPassword).then(function () {
-
-      //   modalRegister.emit({ action: "modal", params: ['close'] });
-    }).catch(function (error) {
-      var errorMessage = error.message;
-
-    });
-  }
+  
   forgetPassword() {
-
-    //var modalForget = this.modalForget;
     firebase.auth().sendPasswordResetEmail(this.forgetEmail).then(function () {
 
-      //  modalForget.emit({ action: "modal", params: ['close'] });
     }, function (error) {
       var errorMessage = error.message;
 
@@ -58,50 +36,20 @@ export class LoginComponent implements OnInit {
   ngOnInit() { }
 
   LoginWithEmail() {
-
     var router = this.router;
     let jwtService: JwtService = this.jwtService;
     firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(function (user) {
-
       var token;
       user = firebase.auth().currentUser.getToken(false).then(function (idToken) {
-        
         token = idToken;
         let jwtRequestObject = new JwtRequest();
-        
         jwtRequestObject.token = token;
-
         jwtService.sendToken(jwtRequestObject).subscribe();
-
         localStorage.setItem('currentUser', user.uid);
-        router.navigate(['/consultarVehiculos']);
-      
+        router.navigate(['/consultar-vehiculos']);
       });
-
-
     }).catch(function (error) {
       var errorMessage = error.message;
-
     });
   }
-
-  loginWithGoogle() {
-
-    var router = this.router;
-    firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(function (result) {
-      var user = result.user;
-      localStorage.setItem('currentUser', user.uid);
-      router.navigate(['/consultarVehiculos']);
-    }).catch(function (error) {
-      var errorMessage = error.message;
-
-    });;
-  }
-
-  enviarToken(uid) {
-
-
-
-  }
-
 }
